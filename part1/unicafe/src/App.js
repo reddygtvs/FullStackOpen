95990c9ac7dc21e03a837c32c7aa52b3010143ce
Tushar <reddygtvs@gmail.com>
2021-09-26 02:25:59 +0530

    part 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,7 +9,25 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <div>
+        <h1>give feedback</h1>
+        <button onClick={() => setGood(good + 1)}>
+          good
+        </button>
+        <button onClick={() => setNeutral(neutral + 1)}>
+          neutral
+        </button>
+        <button onClick={() => setBad(bad + 1)}>
+          bad
+        </button>
+        <h1>statistics</h1>
+      </div>
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+      </div>
+      
     </div>
   )
 }
