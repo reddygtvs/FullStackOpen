f94738a4ec151c9ad6e7d98bdede3d957f24f31e
Tushar <reddygtvs@gmail.com>
2023-05-08 20:15:07 +0530

    part 4.17

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,18 +1,33 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/Blog");
+const User = require("../models/User");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+  });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
+  // const blog = new Blog(request.body);
+  const blog = request.body;
+  const user = await User.findById(blog.userId);
   if (!blog.title || !blog.url) {
     return response.status(400).end();
   }
+  const newBlog = new Blog({
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes,
+    user: user.id,
+  });
 
-  const savedBlog = await blog.save();
+  const savedBlog = await newBlog.save();
+  user.blogs = user.blogs.concat(savedBlog.id);
+  await user.save();
   response.status(201).json(savedBlog);
 });
 
--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -1,14 +1,11 @@
 const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
-const User = require("../models/user");
+const User = require("../models/User");
 
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
   const saltRounds = 10;
   const passwordHash = await bcrypt.hash(password, saltRounds);
-  //   if (username.exists) {
-  //     return response.status(400).json({ error: "username must be unique" });
-  //   }
   const doesUserExist = await User.exists({ username });
   if (doesUserExist) {
     return response.status(400).json({ error: "`username` to be unique" });
@@ -29,7 +26,12 @@ usersRouter.post("/", async (request, response) => {
 });
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    url: 1,
+    title: 1,
+    author: 1,
+    id: 1,
+  });
   response.json(users);
 });
 
