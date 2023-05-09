5ca7676e5824b2e02621adfefb4777b6112ca572
Tushar <reddygtvs@gmail.com>
2023-05-09 13:11:50 +0530

    part 4.18

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const blogsRouter = require("./controllers/blogs");
 const userRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const mongoose = require("mongoose");
 
 const mongoUrl = config.MONGODB_URI;
@@ -13,5 +14,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
