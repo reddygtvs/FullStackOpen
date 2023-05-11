6863285e38a2d0b971bda278e176f83c33ba72ce
Tushar <reddygtvs@gmail.com>
2023-05-11 22:38:00 +0530

    part 5.3

--- part5/bloglist-frontend-main/src/App.js
+++ part5/bloglist-frontend-main/src/App.js
@@ -8,6 +8,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [url, setUrl] = useState("");
   const [errorMessage, setErrorMessage] = useState(null);
   const [user, setUser] = useState(null);
   useEffect(() => {
@@ -46,6 +49,26 @@ const App = () => {
     window.localStorage.clear();
     setUser(null);
   };
+  const handleCreate = async (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title: title,
+      author: author,
+      url: url,
+    };
+    try {
+      const blog = await blogService.create(blogObject);
+      setBlogs(blogs.concat(blog));
+      setTitle("");
+      setAuthor("");
+      setUrl("");
+    } catch (exception) {
+      setErrorMessage("Wrong details");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
   const blogForm = () => (
     <div>
       <h2>Blogs</h2>
@@ -54,6 +77,39 @@ const App = () => {
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
+      <div>
+        <h2>create new</h2>
+        <form onSubmit={handleCreate}>
+          <div>
+            title:
+            <input
+              type="text"
+              value={title}
+              name="Title"
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            author:
+            <input
+              type="text"
+              value={author}
+              name="Author"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url:
+            <input
+              type="text"
+              value={url}
+              name="Url"
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
+      </div>
     </div>
   );
 
--- part5/bloglist-frontend-main/src/services/login.js
+++ part5/bloglist-frontend-main/src/services/login.js
@@ -1,11 +1,6 @@
 import axios from "axios";
 const baseUrl = "http://localhost:3000/api/login";
 
-let token = null;
-
-const setToken = (newToken) => {
-  token = `bearer ${newToken}`;
-};
 const login = async (credentials) => {
   const response = await axios.post(baseUrl, credentials);
   return response.data;
