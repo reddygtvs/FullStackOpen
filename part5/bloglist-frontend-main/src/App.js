a1556e6e82125e6f6c50d6413bcb5ef6e7e1a4b7
Tushar <reddygtvs@gmail.com>
2023-05-15 15:21:53 +0530

    separated components for exercises

--- part5/bloglist-frontend-main/src/App.js
+++ part5/bloglist-frontend-main/src/App.js
@@ -3,6 +3,8 @@ import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
 import Notification from "./components/Notification";
+import Togglable from "./components/Togglable";
+import BlogForm from "./components/BlogForm";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
