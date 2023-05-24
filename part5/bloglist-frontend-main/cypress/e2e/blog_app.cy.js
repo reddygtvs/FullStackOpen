0c92ad98496c31b0e7a801ae9e3185ff171ba155
Tushar <reddygtvs@gmail.com>
2023-05-24 19:40:56 +0530

    part 5.18

--- part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
@@ -1,17 +1,31 @@
 describe("Blog app", function () {
   beforeEach(function () {
     cy.request("POST", "http://localhost:3003/api/testing/reset");
-    // const user = {
-    //   name: "Test User",
-    //   username: "testuser",
-    //   password: "testpassword",
-    // };
-    // cy.request("POST", "http://localhost:3003/api/users/", user);
+    const user = {
+      name: "Tushar Reddy",
+      username: "root",
+      password: "sekret",
+    };
+    cy.request("POST", "http://localhost:3003/api/users/", user);
     cy.visit("http://localhost:3000");
   });
   it("Login form is shown", function () {
     cy.contains("Login");
     cy.contains("username");
     cy.contains("password");
+  });
+  describe("Login", function () {
+    it("succeeds with correct credentials", function () {
+      cy.get("#username").type("root");
+      cy.get("#password").type("sekret");
+      cy.get("#login-button").click();
+      cy.contains("Tushar Reddy logged in");
+    });
+    it("fails with wrong credentials", function () {
+      cy.get("#username").type("root");
+      cy.get("#password").type("wrong");
+      cy.get("#login-button").click();
+      cy.contains("Wrong username or password");
+    });
   });
 });
