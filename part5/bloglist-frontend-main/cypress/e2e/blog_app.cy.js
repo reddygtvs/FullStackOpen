ef6991681d590ee8f75511fda81ec3c268e77ead
Tushar <reddygtvs@gmail.com>
2023-05-24 20:24:11 +0530

    part 5.23

--- part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
@@ -56,7 +56,7 @@ describe("Blog app", function () {
     });
   });
   describe("Only the creator of a blog can delete it", function () {
-    it("A blog can be deleted", function () {
+    beforeEach(function () {
       cy.get("#username").type("root");
       cy.get("#password").type("sekret");
       cy.get("#login-button").click();
@@ -66,20 +66,13 @@ describe("Blog app", function () {
       cy.get("#url").type("Test URL");
       cy.get("#create-button").click();
       cy.contains("Test Blog");
+    });
+    it("A blog can be deleted", function () {
       cy.contains("view").click();
       cy.contains("remove").click();
       cy.contains("Blog deleted");
     });
     it("A blog cannot be deleted by another user", function () {
-      cy.get("#username").type("root");
-      cy.get("#password").type("sekret");
-      cy.get("#login-button").click();
-      cy.contains("New blog").click();
-      cy.get("#title").type("Test Blog");
-      cy.get("#author").type("Test Author");
-      cy.get("#url").type("Test URL");
-      cy.get("#create-button").click();
-      cy.contains("Test Blog");
       cy.contains("logout").click();
       const user = {
         name: "Another User",
@@ -93,6 +86,65 @@ describe("Blog app", function () {
       cy.contains("Test Blog");
       cy.contains("view").click();
       cy.get("remove").should("not.exist");
+    });
+  });
+  describe("Blogs are ordered by likes", function () {
+    beforeEach(function () {
+      cy.get("#username").type("root");
+      cy.get("#password").type("sekret");
+      cy.get("#login-button").click();
+      cy.contains("New blog").click();
+      cy.get("#title").type("Test Blog 1");
+      cy.get("#author").type("Test Author 1");
+      cy.get("#url").type("Test URL 1");
+      cy.get("#create-button").click();
+      cy.contains("New blog").click();
+      cy.get("#title").type("Test Blog 2");
+      cy.get("#author").type("Test Author 2");
+      cy.get("#url").type("Test URL 2");
+      cy.get("#create-button").click();
+      cy.contains("New blog").click();
+      cy.get("#title").type("Test Blog 3");
+      cy.get("#author").type("Test Author 3");
+      cy.get("#url").type("Test URL 3");
+      cy.get("#create-button").click();
+      cy.contains("Test Blog 1");
+      cy.contains("Test Blog 2");
+      cy.contains("Test Blog 3");
+    });
+    it("Blogs are ordered by likes", function () {
+      cy.contains("Test Blog 1").parent().as("blog1");
+      cy.contains("Test Blog 2").parent().as("blog2");
+      cy.contains("Test Blog 3").parent().as("blog3");
+
+      cy.get("@blog1").contains("view").click();
+      cy.get("@blog1").contains("like").click();
+      cy.wait(500);
+      cy.get("@blog1").contains("like").click();
+      cy.wait(500);
+      cy.get("@blog1").contains("like").click();
+      cy.wait(500);
+      cy.get("@blog1").contains("hide").click();
+
+      cy.get("@blog3").contains("view").click();
+      cy.get("@blog3").contains("like").click();
+      cy.wait(500);
+      cy.get("@blog3").contains("like").click();
+      cy.wait(500);
+      cy.get("@blog3").contains("hide").click();
+
+      cy.get("@blog2").contains("view").click();
+      cy.get("@blog2").contains("like").click();
+      cy.wait(500);
+      cy.get("@blog2").contains("hide").click();
+
+      cy.reload();
+
+      cy.get(".Blog").then((blogs) => {
+        cy.wrap(blogs[0]).contains("Test Blog 1");
+        cy.wrap(blogs[1]).contains("Test Blog 3");
+        cy.wrap(blogs[2]).contains("Test Blog 2");
+      });
     });
   });
 });
