39371579d451b3dc0a7bf6f16cc8a02f7d261e6e
Tushar <reddygtvs@gmail.com>
2023-05-24 20:05:34 +0530

    part 5.22

--- part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend-main/cypress/e2e/blog_app.cy.js
@@ -33,27 +33,33 @@ describe("Blog app", function () {
       cy.get("#username").type("root");
       cy.get("#password").type("sekret");
       cy.get("#login-button").click();
-    });
-    it("A blog can be created", function () {
       cy.contains("New blog").click();
       cy.get("#title").type("Test Blog");
       cy.get("#author").type("Test Author");
       cy.get("#url").type("Test URL");
       cy.get("#create-button").click();
+    });
+    it("A blog can be created", function () {
       cy.contains("Test Blog");
     });
     it("A blog can be liked", function () {
-      cy.contains("New blog").click();
-      cy.get("#title").type("Test Blog");
-      cy.get("#author").type("Test Author");
-      cy.get("#url").type("Test URL");
-      cy.get("#create-button").click();
       cy.contains("Test Blog");
       cy.contains("view").click();
       cy.contains("like").click();
       cy.contains("likes: 1");
     });
     it("A blog can be deleted", function () {
+      cy.contains("Test Blog");
+      cy.contains("view").click();
+      cy.contains("remove").click();
+      cy.contains("Blog deleted");
+    });
+  });
+  describe("Only the creator of a blog can delete it", function () {
+    it("A blog can be deleted", function () {
+      cy.get("#username").type("root");
+      cy.get("#password").type("sekret");
+      cy.get("#login-button").click();
       cy.contains("New blog").click();
       cy.get("#title").type("Test Blog");
       cy.get("#author").type("Test Author");
@@ -63,6 +69,30 @@ describe("Blog app", function () {
       cy.contains("view").click();
       cy.contains("remove").click();
       cy.contains("Blog deleted");
+    });
+    it("A blog cannot be deleted by another user", function () {
+      cy.get("#username").type("root");
+      cy.get("#password").type("sekret");
+      cy.get("#login-button").click();
+      cy.contains("New blog").click();
+      cy.get("#title").type("Test Blog");
+      cy.get("#author").type("Test Author");
+      cy.get("#url").type("Test URL");
+      cy.get("#create-button").click();
+      cy.contains("Test Blog");
+      cy.contains("logout").click();
+      const user = {
+        name: "Another User",
+        username: "another",
+        password: "another",
+      };
+      cy.request("POST", "http://localhost:3003/api/users/", user);
+      cy.get("#username").type("another");
+      cy.get("#password").type("another");
+      cy.get("#login-button").click();
+      cy.contains("Test Blog");
+      cy.contains("view").click();
+      cy.get("remove").should("not.exist");
     });
   });
 });
